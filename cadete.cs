958cb8bb8c95e0a2e426edf63c19a4bb1119e758
julian <jnsoretniuq1998@gmail.com>
2023-09-02 23:37:31 -0300

    agrego clase cadete

---
+++ cadete.cs
@@ -0,0 +1,70 @@
+using EspacioPedido;
+using EspacioCadeteria;
+
+namespace EspacioCadete
+{
+    class Cadete
+    {
+        private static int pedidoEntregado = 500;
+        private int id;
+        private string? nombre;
+        private string? direccion;
+        private List<Pedido>listaPedidos; // agregacion
+
+        public static int PedidoEntregado { get => pedidoEntregado; set => pedidoEntregado = value; }
+        public int Id { get => id; set => id = value; }
+        public string? Nombre { get => nombre; set => nombre = value; }
+        public string? Direccion { get => direccion; set => direccion = value; }
+        internal List<Pedido> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+        public Cadete (int id, string nombreCadete, string direccionCadete,List<Pedido>Pedidios)
+        {
+            Id = id;
+            Nombre = nombreCadete;
+            Direccion = direccionCadete;
+            ListaPedidos = Pedidios;
+        } 
+
+        public void AgregarUnPedido (Pedido nuevoPedido) //hace uso del crear pedido de cadeteria 
+        {
+            if (nuevoPedido!=null)ListaPedidos.Add(nuevoPedido);
+        }
+        public void CambiarEstadoPedido(int numeroPedido)
+        {
+            foreach (var pedido in ListaPedidos)
+            {
+                if (pedido.Numero == numeroPedido)
+                {
+                    pedido.CambiarEstado();
+                    break;
+                }
+                
+            }
+    
+        }
+
+        public void EliminarPedido(int numeroPedido)
+        {
+            foreach (var pedido in ListaPedidos)
+             {
+                if (pedido.Numero == numeroPedido)
+                {
+                    ListaPedidos.Remove(pedido);
+                    break;
+                }
+                
+             }
+            
+        }
+        public double JornalACobrar()
+        {
+            double jornal=0;  
+            foreach (var pedido in listaPedidos )
+            {
+                if(pedido.Estado==true) jornal += PedidoEntregado;
+            }
+            return PedidoEntregado;
+        }
+
+    }
+    
+}
