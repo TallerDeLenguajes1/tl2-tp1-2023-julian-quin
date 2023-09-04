04c24423db8ab2e0926c412dda70cbc0736822d6
julian <jnsoretniuq1998@gmail.com>
2023-09-03 23:42:26 -0300

    elimino metodos inecesarios y la lista de pedidos

--- cadete.cs
+++ cadete.cs
@@ -9,13 +9,11 @@ namespace EspacioCadete
         private string? nombre;
         private string? telefono;
         private string? direccion;
-        private List<Pedido>listaPedidos; // agregacion
 
         public static int PedidoEntregado { get => pedidoEntregado; set => pedidoEntregado = value; }
         public int Id { get => id; set => id = value; }
         public string? Nombre { get => nombre; set => nombre = value; }
         public string? Direccion { get => direccion; set => direccion = value; }
-        internal List<Pedido> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
         public string? Telefono { get => telefono; set => telefono = value; }
 
         public Cadete (int id, string nombreCadete, string direccionCadete,string TelCadete)
@@ -24,50 +22,9 @@ namespace EspacioCadete
             Nombre = nombreCadete;
             Direccion = direccionCadete;
             Telefono = TelCadete;
-            listaPedidos = new List<Pedido>();
         }
         public Cadete(){}
 
-        public void AgregarUnPedido (Pedido nuevoPedido) //hace uso del crear pedido de cadeteria 
-        {
-            ListaPedidos.Add(nuevoPedido);
-        }
-        public void CambiarEstadoPedido(int numeroPedido)
-        {
-            foreach (var pedido in ListaPedidos)
-            {
-                if (pedido.Numero == numeroPedido)
-                {
-                    pedido.CambiarEstado();
-                    break;
-                }
-                
-            }
-    
-        }
-
-        public void EliminarPedido(int numeroPedido)
-        {
-            foreach (var pedido in ListaPedidos)
-             {
-                if (pedido.Numero == numeroPedido)
-                {
-                    ListaPedidos.Remove(pedido);
-                    break;
-                }
-                
-             }
-            
-        }
-        public double JornalACobrar()
-        {
-            double jornal=0;  
-            foreach (var pedido in listaPedidos )
-            {
-                if(pedido.Estado==true) jornal += PedidoEntregado;
-            }
-            return PedidoEntregado;
-        }
 
     }
     
