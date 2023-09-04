51c747ff5d69ced14bb6037c562c2030113d8070
julian <jnsoretniuq1998@gmail.com>
2023-09-04 18:34:40 -0300

    corrijo el retorno del metodo jornal()

--- cadete.cs
+++ cadete.cs
@@ -66,7 +66,7 @@ namespace EspacioCadete
             {
                 if(pedido.Estado==true) jornal += PedidoEntregado;
             }
-            return PedidoEntregado;
+            return jornal;
         }
 
     }
