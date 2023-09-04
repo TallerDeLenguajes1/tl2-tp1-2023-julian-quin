d262790839ca0c447ad3031d3d62d3a13a35bcf3
julian <jnsoretniuq1998@gmail.com>
2023-09-03 23:46:56 -0300

    agrego una opcion de asignar cad. a ped. y adapto la interfaz

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ internal class Program
         string []DatosCad = HerramientasCsv.ObtenerDatosCadeteria("DatosCadeteria.csv");
         var variableCadeteria = new Cadeteria(DatosCad[0],DatosCad[1],listaCadetes);
         int option;
-        const int MaxOption = 4;
+        const int MaxOption = 5;
         const bool P_No_Entregado = false;
         string? observacionPedido,nombreCliente, direccionCliente,telCliente,refDireccionCliente;
         int numeroPedido, idCadete; 
@@ -21,6 +21,7 @@ internal class Program
             switch (option)
             {
                 case 1:
+                    Console.WriteLine("\nALTA PEDIDO\n");
                     Console.WriteLine("Acerca del cliente\n");
                     Console.WriteLine("Nombe: ");
                     nombreCliente = Console.ReadLine();
@@ -35,27 +36,34 @@ internal class Program
                     ValidarNumero(out numeroPedido);
                     Console.WriteLine("Obs pedido: ");
                     observacionPedido = Console.ReadLine();
-                    Console.WriteLine("Indique el id del cadete para asignar el pedido");
-                    ValidarNumero(out idCadete);
-                    if(variableCadeteria.CrearPedido(numeroPedido,observacionPedido,P_No_Entregado,nombreCliente,direccionCliente,telCliente,refDireccionCliente,idCadete))Console.WriteLine("Pedido creado y asignado correctamente");
-                    else Console.WriteLine("Error: id no encontrado");
+                    if(variableCadeteria.CrearPedido(numeroPedido,observacionPedido,P_No_Entregado,nombreCliente,direccionCliente,telCliente,refDireccionCliente))Console.WriteLine("Pedido creado correctamente\n");
+                    else Console.WriteLine("Error al crear el pedido\n");
                 break;
                 case 2:
-
-                    Console.WriteLine("\nCAMBIAR DE ESTADO UN PEDIDO\n");
-                    Console.WriteLine("Ingrese el numero de pedido\n");
-                    ValidarNumero(out numeroPedido);
-                    if(variableCadeteria.CambiarEstadoPedido(numeroPedido))Console.WriteLine("Estado del pedido Cambiado");
-                    else Console.WriteLine("numero de pedido invalido");
-                break;
-                case 3:
-                    Console.WriteLine("\nREASIGNAR PEDIDO A OTRO CADETE\n");
+                    Console.WriteLine("\nASIGNAR CADETE A UN PEDIDO\n");
                     Console.WriteLine("Ingrese el numero de pedido\n");
                     ValidarNumero(out numeroPedido);
                     Console.WriteLine("Ingrese el numero ID del cadete\n");
                     ValidarNumero(out idCadete);
-                    if(variableCadeteria.ReasignarPedido(idCadete,numeroPedido)) Console.WriteLine("Pedido Reasignado");
-                    else Console.WriteLine("numero de pedido o identificador invalido");
+                    if(variableCadeteria.AsignarCadeteAPedido(idCadete,numeroPedido)) Console.WriteLine("Asignacion Exitosa\n");
+                    else Console.WriteLine("Error: cadete o pedido no encontrado\n");
+                    
+                break;
+                case 3:
+                    Console.WriteLine("\nCAMBIAR DE ESTADO UN PEDIDO\n");
+                    Console.WriteLine("Ingrese el numero de pedido\n");
+                    ValidarNumero(out numeroPedido);
+                    if(variableCadeteria.CambiarEstadoPedido(numeroPedido))Console.WriteLine("Estado del pedido Cambiado\n");
+                    else Console.WriteLine("numero de pedido invalido\n");
+                break;
+                case 4:
+                    Console.WriteLine("\nREASIGNAR CADETE A UN PEDIDO\n");
+                    Console.WriteLine("Ingrese el numero de pedido\n");
+                    ValidarNumero(out numeroPedido);
+                    Console.WriteLine("Ingrese el numero ID del cadete\n");
+                    ValidarNumero(out idCadete);
+                    if(variableCadeteria.ReasignarCadeteApedido(idCadete,numeroPedido)) Console.WriteLine("Nuevo cadete reasignado al pedido\n");
+                    else Console.WriteLine("numero de pedido o identificador invalido\n");
 
                 break;
 
@@ -67,9 +75,10 @@ internal class Program
         {
             bool flag;
             Console.WriteLine("\t1 ALTA PEDIDO");
-            Console.WriteLine("\t2 CAMBIAR ESTADO PEDIDOS");
-            Console.WriteLine("\t3 REASIGNAR PEDIDOS");
-            Console.WriteLine("\t4 SALIR");
+            Console.WriteLine("\t2 ASIGNAR CADETE A UN PEDIDO");
+            Console.WriteLine("\t3 CAMBIAR ESTADO PEDIDOS");
+            Console.WriteLine("\t4 REASIGNAR CADETE A UN PEDIDO");
+            Console.WriteLine("\t5 SALIR");
             
             do
             {
